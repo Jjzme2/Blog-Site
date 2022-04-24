fc3414f199a89f5a5b3b5ce7f8762ce06534f826
Jj <Jjzettler@gmail.com>
2022-04-24 09:23:51 -0500

    Added MongoDB

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -5,52 +6,90 @@ const date = require(__dirname + "/date.js")
 const _ = require('lodash');
 const app = express();
 
-
+const discordInfo = "https://discord.gg/cE53DwRt";
 const dayContent = date.getTimeOfDay();
+const contactInfo = "Jjzettler@Gmail.com";
 const homeContent = "welcome to ILYTATWrites.";
 const aboutContent = "This is the about section Content.";
-const contactContent = "Contact me here: ";
+const contactContent = "If you are interested in reaching me, feel free to contact me at";
 
-// Let is safer
-var posts = [];
 
 let postRedir = "";
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(express.static("public"));
 
 
+
+const connectLink = "mongodb+srv://Jjzme2:tBYrKITMQ8RNwcjl@blog-site.t39wt.mongodb.net/posts?retryWrites=true&w=majority";
+
+
+mongoose.connect('mongodb://localhost:27017/blogDB', { useNewUrlParser: true });
+
+
+// Schemas
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false,
+    }
+});
+
+//Models
+const Post = mongoose.model("Post", PostSchema);
+
+//Initial Array
+const post1 = new Post({ title: "My First Post", body: "This is a post", description: "Really just a test" });
+const post2 = new Post({ title: "Second Post Here", body: "Hello World!", description: "Second Description" });
+const post3 = new Post({ title: "Third Post Here", body: "Goodbye World!", description: "Third Description" });
+
+const posts = [post1, post2, post3];
+
+
+// App Get
+
 app.get("/", function(req, res) {
-  res.render('home', {
-    homeContent: homeContent,
-    timeOfDay: dayContent,
-    posts: posts
-  });
+    res.render('home', {
+        content: homeContent,
+        timeOfDay: dayContent,
+        posts: posts
+    });
 });
 
 app.get("/about", function(req, res) {
-  res.render('about', {
-    homeContent: aboutContent,
-    timeOfDay: dayContent
-  });
+    res.render('about', {
+        content: aboutContent,
+        timeOfDay: dayContent
+    });
 });
 
 app.get("/contact", function(req, res) {
-  res.render('contact', {
-    homeContent: contactContent,
-    timeOfDay: dayContent
-  });
+    res.render('contact', {
+        content: contactContent,
+        timeOfDay: dayContent,
+        email: contactInfo,
+        discord: discordInfo,
+    });
 });
 
 app.get("/compose", function(req, res) {
-  res.render('compose', {
-    homeContent: contactContent,
-    timeOfDay: dayContent
-  });
+    res.render('compose', {
+        // homeContent: contactContent,
+        timeOfDay: dayContent
+    });
 });
 
 // app.get('/post', function(req, res) {
@@ -77,31 +116,31 @@ app.get("/compose", function(req, res) {
 
 
 //My Attempt
-app.get('/posts/:post', function (req, res) {
+app.get('/posts/:post', function(req, res) {
 
-  let post = _.lowerCase(req.params.post);
+    let post = _.lowerCase(req.params.post);
 
-  posts.forEach(function(p) {
-      if(post === _.lowerCase(p.title))
-      {
-        res.render(`post`, {
-          postTitle: p.title,
-          postBody: p.body
-        });
-      }
-  })
+    posts.forEach(function(p) {
+        if (post === _.lowerCase(p.title)) {
+            res.render(`post`, {
+                // content: p.description,
+                postTitle: p.title,
+                postBody: p.body
+            });
+        }
+    })
 })
 
-    app.post("/compose", function(req, res) {
-      let post = {
+app.post("/compose", function(req, res) {
+    let post = {
         title: req.body.postTitle,
         body: req.body.postBody
-      };
+    };
 
-      posts.push(post);
-      res.redirect("/");
-    });
+    posts.push(post);
+    res.redirect("/");
+});
 
-    app.listen(3000, function(req, res) {
-      console.log("Server started on port 3000.");
-    });
+app.listen(3000, function(req, res) {
+    console.log("Server started on port 3000.");
+});
